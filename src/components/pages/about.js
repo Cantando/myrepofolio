e1adaf15ae8b182d631b2581df4dae9070b6dd26
Cantando <65988365+Cantando@users.noreply.github.com>
2020-08-14 22:54:03 -0400

    finalized the about page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -7,14 +7,29 @@ function About() {
 
     return (
         <>
-            <h1>About</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <h1>This is me!</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-2">
+                        <img src={Me} alt="myimage" className="img-thumbnail me" />
+                    </div>
+                    <div className="col-sm-10">
+                        <p>
+                            Hi! My name is Myrna Cantando. I am a full stack developer, and I'm very passionate and dedicated to my work and I am always open to learning new technologies and frameworks. I am an ambitious and exceptionally organized developer who is recognized for the ability to communicate effectively, work as part of a team and solve problems creatively. I am active in my community and enjoy giving back. I have a strong background in Real Estate and Mortgages. I have the ability to think through problem coupled with the confidence to make ideas heard. I believe that "Success is a journey, not a destination".
+                        </p>
+                    </div>
+                </div>
+            </div>
 
 
-            <img src={Me} alt="myimage" className="img-thumbnail me" />
 
-            <p>
 
-            </p>
+
+
 
 
 
