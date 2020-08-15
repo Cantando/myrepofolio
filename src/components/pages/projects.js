5368da4dd9cf774bb36b5bdae7388464b5c20aa9
Cantando <65988365+Cantando@users.noreply.github.com>
2020-08-15 00:18:48 -0400

    added bootstrap cards imported two project images and resume

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -1,12 +1,78 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import project1 from "../../images/project1.png"
+import notetaker from "../../images/Notetaker.png"
 
-
-class Projects extends Component{
+class Projects extends Component {
     render() {
-        return(
-            <div><h1>Projects</h1></div>
+        return (
+            <div className="container-fluid">
+                <div><h1>Projects</h1></div>
+
+                <div className="row">
+                    <div className="col-sm-12">
+                        <div className="card-deck">
+                            <div className="card">
+                                <img src={project1} className="card-img-top" alt="project1" />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <img src={notetaker} className="card-img-top" alt="notetaker" />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <img src="..." className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-12">
+                        <div className="card-deck">
+                            <div className="card">
+                                <img src="..." className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <img src="..." className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <img src="..." className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
 
         )
-        }
+    }
 }
 export default Projects;
--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -1,12 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import resume from "../../images/Myrna_Cantando_Resume.pdf"
 
 
-class Resume extends Component{
+class Resume extends Component {
     render() {
-        return(
-            <div><h1>Resume Page</h1></div>
-
+        return (
+        <>
+            <div>
+            <a href={resume} target="_blank" rel="noopener noreferrer"><h1>Resume Page</h1> </a>
+            </div>
+        </>
         )
-        }
+    }
 }
 export default Resume;
