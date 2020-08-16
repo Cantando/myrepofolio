d6c812bfd28f0ba469f0279d9742eedb46b88b0f
Cantando <65988365+Cantando@users.noreply.github.com>
2020-08-16 15:53:39 -0400

    Updates

---
+++ precache-manifest.ea353e269dfdea6033d494f6eef9bb0d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9279e1a47177f0418ed58c4e029430",
+    "url": "/myrepofolio/index.html"
+  },
+  {
+    "revision": "67437b5d39c1059370e1",
+    "url": "/myrepofolio/static/css/main.c542b1f0.chunk.css"
+  },
+  {
+    "revision": "a9208b188152f803cf90",
+    "url": "/myrepofolio/static/js/2.0a906937.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myrepofolio/static/js/2.0a906937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67437b5d39c1059370e1",
+    "url": "/myrepofolio/static/js/main.be09e858.chunk.js"
+  },
+  {
+    "revision": "298373931319d108fb00",
+    "url": "/myrepofolio/static/js/runtime-main.366ef093.js"
+  },
+  {
+    "revision": "48b1343ca77c025d966b0eb4a2dec3ce",
+    "url": "/myrepofolio/static/media/Generatepassword.48b1343c.png"
+  },
+  {
+    "revision": "003281c9183931d6ba710ca808b5251d",
+    "url": "/myrepofolio/static/media/Myrna_Cantando_Resume.003281c9.pdf"
+  },
+  {
+    "revision": "418ef76a1e9b6bf0c2df4384449fae6b",
+    "url": "/myrepofolio/static/media/Notetaker.418ef76a.png"
+  },
+  {
+    "revision": "e7c24c1703511277e6bcda433a46e080",
+    "url": "/myrepofolio/static/media/Project2.e7c24c17.png"
+  },
+  {
+    "revision": "0f9e7a321bebf9bc315bb581ea192c11",
+    "url": "/myrepofolio/static/media/Quizgame.0f9e7a32.png"
+  },
+  {
+    "revision": "c471c7ef17f34f299fa4a6a8e1ca6230",
+    "url": "/myrepofolio/static/media/Weatherapp.c471c7ef.png"
+  },
+  {
+    "revision": "8c952ec7f856212de8043ec60f855b0c",
+    "url": "/myrepofolio/static/media/me.8c952ec7.JPG"
+  },
+  {
+    "revision": "a284759d95d965ab9eb010c60e4826c9",
+    "url": "/myrepofolio/static/media/project1.a284759d.png"
+  }
+]);
